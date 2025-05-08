393703c3cdd34c4f1f3cbfb8dbb0ab4185db1d26
Mattb2291 <mattb2291@aol.com>
2025-05-09 00:10:14 +0100

    Glassman v1a

--- src/main/java/com/Glassman/GlassmanPlugin.java
+++ src/main/java/com/Glassman/GlassmanPlugin.java
@@ -1,17 +1,13 @@
 package com.Glassman;
 
-import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
+import net.runelite.api.*;
 import net.runelite.api.coords.WorldArea;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.HitsplatApplied;
 import net.runelite.api.events.PlayerSpawned;
 import net.runelite.api.gameval.InterfaceID;
-import net.runelite.api.Skill;
-import net.runelite.api.SpritePixels;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -39,30 +35,23 @@ public class GlassmanPlugin extends Plugin
 	private Client client;
 
 	@Inject
-	private GlassmanConfig config;
-
-	@Inject
 	private ConfigManager configManager;
 
 	private boolean playerIsFragile = true;
-	private final HashSet<WorldArea> tutorialIslandWorldArea = new HashSet<WorldArea>();
+	private final String GLASSMANCONFIGGROUP = "GLASSMAN";
+	private final String GLASSMANVALID = "VALID";
+	private final String GLASSMANTIMER = "TIMER";
 
-	@Provides
-	GlassmanConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(GlassmanConfig.class);
-	}
-
-	@Override
-	protected void startUp() throws Exception
-	{
-		getTutorialIslandWorldAreas();
-	}
+	private final HashSet<WorldArea> tutorialIslandWorldArea = new HashSet<WorldArea>(){{
+		add(new WorldArea(3053, 3072,103,64,0));  // RegionIDs: 12080, 12336 and 12592
+		add(new WorldArea(3059, 3051,77,21,0));  // RegionIDs: 12079 and 12335
+		add(new WorldArea(3072, 9493,45,41,0));  // RegionID: 12436
+	}};
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		tutorialIslandWorldArea.clear();
+		restoreGame(false);
 	}
 
 	@Subscribe
@@ -79,15 +68,15 @@ public class GlassmanPlugin extends Plugin
 						sendGamemodeMessage("You begin to feel fragile... as though with just one hit your journey" +
 								" will be over and your heart will shatter. How far will you get?",	Color.MAGENTA);
 						playerIsFragile = true;
-						setPlayerConfig(GlassmanConfig.timePlayer,Instant.now().toString() );
-						setPlayerConfig(GlassmanConfig.validPlayer,Boolean.toString(playerIsFragile));
+						setPlayerConfig(GLASSMANTIMER,Instant.now().toString() );
+						setPlayerConfig(GLASSMANVALID,Boolean.toString(playerIsFragile));
 					}
 				}
 				else
 				{
 					sendGamemodeMessage("You have previously entered combat and are ineligible to be a Glassman.",
 							Color.RED);
-					restoreGame();
+					restoreGame(true);
 				}
 			}
 			else
@@ -95,91 +84,15 @@ public class GlassmanPlugin extends Plugin
 				playerIsFragile = isPlayerFragile();
 				if (!playerIsFragile)
 				{
-					sendGamemodeMessage("You are not eligible to be a Glassman.",
-							Color.RED);
-					restoreGame();
+					sendGamemodeMessage("You are not eligible to be a Glassman.", Color.RED);
+					restoreGame(true);
 				}
 			}
 		}
 	}
 
-	private long getCombatExperience()
-	{
-		return client.getSkillExperience(Skill.ATTACK) + client.getSkillExperience(Skill.STRENGTH) +
-				client.getSkillExperience(Skill.DEFENCE) + client.getSkillExperience(Skill.MAGIC) +
-				client.getSkillExperience(Skill.RANGED);
-	}
-
-	private boolean isPlayerFragile()
-	{
-		String playerString = getPlayerConfig(GlassmanConfig.validPlayer);
-		if (playerString == null) {return false;}
-		return Boolean.parseBoolean(playerString);
-	}
-
-	private void removePlayerFromFragileMode()
-	{
-		setPlayerConfig(GlassmanConfig.validPlayer,null);
-	}
-
-	private void setPlayerConfig(String key, Object value)
-	{
-		if (value != null)
-		{
-			configManager.setRSProfileConfiguration(GlassmanConfig.CONFIGGROUP, key, value);
-		}
-		else
-		{
-			configManager.unsetRSProfileConfiguration(GlassmanConfig.CONFIGGROUP, key);
-		}
-	}
-	private String getPlayerConfig(String key)
-	{
-		return configManager.getRSProfileConfiguration(GlassmanConfig.CONFIGGROUP, key);
-	}
-
-	private void sendGamemodeMessage(String msg, Color formatColor)
-	{
-		String message = ColorUtil.wrapWithColorTag(String.format(msg),formatColor);
-		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", message, null);
-	}
-
-	private void getTutorialIslandWorldAreas()
-	{
-		if (tutorialIslandWorldArea.isEmpty())
-		{
-			// RegionIDs: 12080, 12336 and 12592
-			tutorialIslandWorldArea.add(new WorldArea(3053, 3072,103,64,0));
-			// RegionIDs: 12079 and 12335
-			tutorialIslandWorldArea.add(new WorldArea(3059, 3051,77,21,0));
-			// RegionID: 12436 - The basement/dungeon where combat is learned
-			tutorialIslandWorldArea.add(new WorldArea(3072, 9493,45,41,0));
-		}
-	}
-
-	private boolean locationIsOnTutorialIsland(WorldPoint playerLocation){
-		for (WorldArea worldArea : tutorialIslandWorldArea)
-		{
-			if (worldArea.contains2D(playerLocation))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private void restoreGame()
-	{
-		playerIsFragile = false;
-		removePlayerFromFragileMode();
-		setHPOrbText(client.getBoostedSkillLevel(Skill.HITPOINTS));
-		setHPStatText(client.getBoostedSkillLevel(Skill.HITPOINTS), client.getRealSkillLevel(Skill.HITPOINTS), false);
-		restoreSprites();
-	}
-
 	@Subscribe
-	public void onGameTick(GameTick t)
-	{
+	public void onGameTick(GameTick t) {
 		if (playerIsFragile)
 		{
 			setHPOrbText(1);
@@ -194,20 +107,74 @@ public class GlassmanPlugin extends Plugin
 
 		if (damage.getHitsplat().getAmount() > 0 && playerIsFragile)
 		{
-
 			sendGamemodeMessage("And with one blow, your fragile heart shatters. Having taken damage, you are no" +
 					" longer worthy of Glassman status...", Color.RED);
 
 			sendGamemodeMessage("You were a Glassman for a duration of: " +
 					getTimeFragile() + " without taking damage!", Color.RED);
 
-			restoreGame();
+			restoreGame(true);
 		}
 	}
 
+	private long getCombatExperience()
+	{
+		return client.getSkillExperience(Skill.ATTACK) + client.getSkillExperience(Skill.STRENGTH) +
+				client.getSkillExperience(Skill.DEFENCE) + client.getSkillExperience(Skill.MAGIC) +
+				client.getSkillExperience(Skill.RANGED);
+	}
+
+	private boolean isPlayerFragile()
+	{
+		String playerString = getPlayerConfig(GLASSMANVALID);
+		if (playerString == null) {return false;}
+		return Boolean.parseBoolean(playerString);
+	}
+
+	private void removePlayerFromFragileMode()
+	{
+		setPlayerConfig(GLASSMANVALID,false);
+	}
+
+	private void setPlayerConfig(String key, Object value) {
+		if (value != null) {
+			configManager.setRSProfileConfiguration(GLASSMANCONFIGGROUP, key, value);
+		}
+		else {
+			configManager.unsetRSProfileConfiguration(GLASSMANCONFIGGROUP, key);
+		}
+	}
+
+	private String getPlayerConfig(String key) {
+		return configManager.getRSProfileConfiguration(GLASSMANCONFIGGROUP, key);
+	}
+
+	private void sendGamemodeMessage(String msg, Color formatColor)	{
+		String message = ColorUtil.wrapWithColorTag(String.format(msg),formatColor);
+		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", message, null);
+	}
+
+	private boolean locationIsOnTutorialIsland(WorldPoint playerLocation) {
+		for (WorldArea worldArea : tutorialIslandWorldArea) {
+			if (worldArea.contains2D(playerLocation)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void restoreGame(boolean removePlayerFromGamemode) {
+		playerIsFragile = false;
+		if (removePlayerFromGamemode) {removePlayerFromFragileMode();}
+		setHPOrbText(client.getBoostedSkillLevel(Skill.HITPOINTS));
+		setHPStatText(client.getBoostedSkillLevel(Skill.HITPOINTS), client.getRealSkillLevel(Skill.HITPOINTS), false);
+		restoreSprites();
+	}
+
+
 	private String getTimeFragile()
 	{
-		Duration durationFragile = Duration.between(Instant.parse(getPlayerConfig(GlassmanConfig.timePlayer)), Instant.now());
+		Duration durationFragile = Duration.between(Instant.parse(getPlayerConfig(GLASSMANTIMER)), Instant.now());
 		return durationFragile.toDaysPart() + " days and " +
 				durationFragile.toHoursPart() + " hours and " +
 				durationFragile.toMinutesPart() + " minutes.";
@@ -219,7 +186,7 @@ public class GlassmanPlugin extends Plugin
 		{
 			try
 			{
-				final String spriteFile = "../../" + spriteOverride.getSpriteID() + ".png";
+				final String spriteFile = "/" + spriteOverride.getSpriteID() + ".png";
 				BufferedImage spriteImage = ImageUtil.getResourceStreamFromClass(getClass(), spriteFile);
 				SpritePixels spritePixels = ImageUtil.getImageSpritePixels(spriteImage, client);
 				client.getSpriteOverrides().put(spriteOverride.getSpriteID(), spritePixels);
@@ -242,7 +209,7 @@ public class GlassmanPlugin extends Plugin
 
 	private void setHPOrbText(int levelToDisplay)
 	{
-		Widget HPTextWidget = client.getWidget(InterfaceID.Orbs.HEALTH_TEXT);
+		Widget HPTextWidget = client.getWidget(InterfaceID.ORBS,9);
 		if (HPTextWidget != null)
 		{
 			HPTextWidget.setText(Integer.toString(levelToDisplay));
@@ -251,11 +218,10 @@ public class GlassmanPlugin extends Plugin
 
 	private void setHPStatText(int topStatLevel, int bottomStatLevel, boolean disableHitpointsListener)
 	{
-		Widget HPStatWidget = client.getWidget(net.runelite.api.widgets.InterfaceID.SKILLS, 9);
+		Widget HPStatWidget = client.getWidget(InterfaceID.STATS, 9);
 		if (HPStatWidget != null) {
 			HPStatWidget.setHasListener(disableHitpointsListener);
 			Widget[] HPStatWidgetComponents = HPStatWidget.getDynamicChildren();
-			HPStatWidgetComponents[2].getSpriteId();
 			HPStatWidgetComponents[3].setText(Integer.toString(topStatLevel));
 			HPStatWidgetComponents[4].setText(Integer.toString(bottomStatLevel));
 		}
